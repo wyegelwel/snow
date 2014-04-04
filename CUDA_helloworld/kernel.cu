949a51fd3dd9e4a81497a58f3a4ad06142ea1a1c
Eric Jang <evjang@cs.brown.edu>
2014-04-04 17:09:55 -0400

    test CUDA QT project. has problems with LD_LIBRARY_PATH for libcudart

---
+++ CUDA_helloworld/kernel.cu
@@ -0,0 +1,14 @@
+#include <cuda.h>
+//#include <cutil_inline.h>
+#include <glm/glm.hpp>
+//#include "cuPrintf.h"
+
+extern "C"
+void runCudaPart();
+
+void runCudaPart()
+{
+    // all your cuda code here
+//    cuPrintf("Hello, world from the device!\n");
+    glm::vec4 v(0.0f);
+}
---
+++ CUDA_helloworld/main.cpp
@@ -0,0 +1,15 @@
+#include <QCoreApplication>
+
+extern "C" // tell compiler that function is defined somewhere else
+
+void runCudaPart();
+
+
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    runCudaPart();
+    return a.exec();
+}
