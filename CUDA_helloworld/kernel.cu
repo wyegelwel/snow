ca2802f65dffb44295589fb9c3c1853a7a36c0ee
Eric Jang <evjang@cs.brown.edu>
2014-04-04 17:37:12 -0400

    fix

--- CUDA_helloworld/kernel.cu
+++ CUDA_helloworld/kernel.cu
@@ -1,14 +1,19 @@
 #include <cuda.h>
-//#include <cutil_inline.h>
 #include <glm/glm.hpp>
-//#include "cuPrintf.h"
+#include <stdio.h>
+
+
+#if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 200)
+  # error printf is only supported on devices of compute capability 2.0 and higher, please compile with -arch=sm_20 or higher
+#endif
 
 extern "C"
 void runCudaPart();
 
+
 void runCudaPart()
 {
     // all your cuda code here
-//    cuPrintf("Hello, world from the device!\n");
+    printf("Hello, world from the device!");
     glm::vec4 v(0.0f);
 }
--- CUDA_helloworld/main.cpp
+++ CUDA_helloworld/main.cpp
@@ -4,8 +4,7 @@ extern "C" // tell compiler that function is defined somewhere else
 
 void runCudaPart();
 
-
-
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
@@ -13,3 +12,16 @@ int main(int argc, char *argv[])
     runCudaPart();
     return a.exec();
 }
+
+
+//__global__ void helloCUDA(float f)
+//{
+//    printf("Hello thread %d, f=%f\n", threadIdx.x, f);
+//}
+
+//int main()
+//{
+//    helloCUDA<<<1, 5>>>(1.2345f);
+//    cudaDeviceSynchronize();
+//    return 0;
+//}
