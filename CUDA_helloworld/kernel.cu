78d8c725bdabecc109112d470e9e923f6f00dc1e
Eric Jang <evjang@cs.brown.edu>
2014-04-04 17:39:25 -0400

    working hello world example

--- CUDA_helloworld/kernel.cu
+++ CUDA_helloworld/kernel.cu
@@ -10,10 +10,15 @@
 extern "C"
 void runCudaPart();
 
+__global__ void helloCUDA(float f)
+{
+    printf("Hello thread %d, f=%f\n", threadIdx.x, f);
+}
 
 void runCudaPart()
 {
     // all your cuda code here
-    printf("Hello, world from the device!");
+    helloCUDA<<<1, 5>>>(1.2345f);
+    cudaDeviceSynchronize();
     glm::vec4 v(0.0f);
 }
--- CUDA_helloworld/main.cpp
+++ CUDA_helloworld/main.cpp
@@ -14,14 +14,4 @@ int main(int argc, char *argv[])
 }
 
 
-//__global__ void helloCUDA(float f)
-//{
-//    printf("Hello thread %d, f=%f\n", threadIdx.x, f);
-//}
 
-//int main()
-//{
-//    helloCUDA<<<1, 5>>>(1.2345f);
-//    cudaDeviceSynchronize();
-//    return 0;
-//}
