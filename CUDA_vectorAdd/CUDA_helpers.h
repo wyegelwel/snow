4f646fa0089f0dfab773e7a516bbd9645558b6b5
Eric Jang <evjang@cs.brown.edu>
2014-04-06 16:57:58 -0400

    vector addition example

---
+++ CUDA_vectorAdd/CUDA_helpers.h
@@ -0,0 +1,16 @@
+#ifndef CUDA_HELPERS_H
+#define CUDA_HELPERS_H
+
+// stolen from CUDA by example book
+static void HandleError( cudaError_t err,
+                         const char *file,
+                         int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+#endif // CUDA_HELPERS_H
---
+++ CUDA_vectorAdd/main.cpp
@@ -0,0 +1,14 @@
+#include <QCoreApplication>
+
+extern "C" // tell compiler that function is defined somewhere else
+
+void runCudaPart();
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    runCudaPart();
+    return a.exec();
+}
