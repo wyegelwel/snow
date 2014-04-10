8d7e9e28c8d10c8a6332d92a0d537cbcc2b13e4a
Wil Yegelwel <wyegelwel@gmail.com>
2014-04-10 15:36:28 -0400

    Fixed objparser.cpp so that it works with newer versions of qmake / qt. toAscii() has been removed in favor of toLatin1()

---
+++ project/sim/collider.cpp
@@ -0,0 +1,5 @@
+#include "collider.h"
+
+Collider::Collider()
+{
+}
---
+++ project/sim/collider.h
@@ -0,0 +1,10 @@
+#ifndef COLLIDER_H
+#define COLLIDER_H
+
+class Collider
+{
+public:
+    Collider();
+};
+
+#endif // COLLIDER_H
