73717a2721e86199fe038965a6f715cd1ff50330
Wil Yegelwel <wyegelwel@gmail.com>
2014-04-14 18:02:44 -0400

    Realized I forgot to commit the glm/ folder. Also, created a struct for world paramaters, such as Young's modulus and Coefficient of friction

---
+++ project/sim/world.h
@@ -0,0 +1,10 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+struct WorldParams{
+    float lambda; // first Lame parameter
+    float mu; //second Lame paramter
+    float xi; // Plastic hardening parameter
+};
+
+#endif // WORLD_H
