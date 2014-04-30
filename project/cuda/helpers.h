dc54db87f2bcce2ca2f4758b9b8d8614f78c144e
Wil Yegelwel <wyegelwel@gmail.com>
2014-04-30 18:25:35 -0400

    Added a couple macros to helpers.cu

--- project/cuda/helpers.h
+++ project/cuda/helpers.h
@@ -18,6 +18,22 @@
 
 #define LAUNCH( ... ) { __VA_ARGS__; checkCudaErrors( cudaDeviceSynchronize() ); }
 
+#define cudaMallocAndCopy( dst, src, size )                    \
+({                                                             \
+    cudaMalloc((void**) &dst, size);                           \
+    cudaMemcpy(dst, src, size, cudaMemcpyHostToDevice);        \
+})
+
+#define TEST( toCheck, msg, failExprs)      \
+({                                          \
+    if (toCheck){                           \
+        printf("[PASSED]: %s\n", msg);      \
+    }else{                                  \
+        printf("[FAILED]: %s\n", msg);      \
+        failExprs;                          \
+    }                                       \
+})
+
 #define THREAD_COUNT 128
 
 #endif // HELPERS_H
